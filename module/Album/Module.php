3206e47d3291a27f810c9602c1b19ee127c13da2
Dani Gonzalez <dgonzalez@inthe.es>
2013-03-19 11:53:41 +0100

    Add service configuration to the Album module

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -2,6 +2,10 @@
 namespace Album;
 
 use Zend\Mvc\ModuleRouteListener;
+use Album\Model\Album;
+use Album\Model\AlbumTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
 use Zend\Mvc\MvcEvent;
 
 class Module
@@ -23,5 +27,24 @@ class Module
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+    
+    public function getServiceConfig()
+    {
+    	return array(
+    			'factories' => array(
+    					'Album\Model\AlbumTable' =>  function($sm) {
+    						$tableGateway = $sm->get('AlbumTableGateway');
+    						$table = new AlbumTable($tableGateway);
+    						return $table;
+    					},
+    					'AlbumTableGateway' => function ($sm) {
+    						$dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+    						$resultSetPrototype = new ResultSet();
+    						$resultSetPrototype->setArrayObjectPrototype(new Album());
+    						return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+    					},
+    			),
+    	);
     }
 }
